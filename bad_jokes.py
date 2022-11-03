0b4ec5cc55deab121fd0bba42d935b5b5c5b66ae
Pedro Echavarria <pechavarria62@gmail.com>
2022-11-03 13:05:19 -0400

    updates

--- bad_jokes.py
+++ bad_jokes.py
@@ -1,1 +1,11 @@
 # Bad Jokes by Pedro Echavarria.
+
+text_file = "bad_jokes.txt"
+lol = "Hee hee hee!, ha ha ha!, ho ho ho!, ha ha ha!, ho ho ho!"
+bad_files = open(text_file)
+def bad_jokes():
+    for i in bad_files:
+        line = i.rstrip().split(',')
+        print(input(line[0]))
+        # print(line)
+bad_jokes()
